c25b9a5e271478ca78bc21817c5a7fce78374f28
Cássio Capucho <cassiocpecanha@gmail.com>
2026-05-03 18:33:14 +0000

    Config Incial

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Imprimindo linha 01.\n");
+    printf("Imprimindo linha 02.\n");
+    printf("Imprimindo linha 03.\n");
+    printf("Imprimindo linha 04.\n");
+    printf("Imprimindo linha 05.\n");
+    printf("Imprimindo linha 06.\n");
+    printf("Imprimindo linha 07.\n");
+    printf("Imprimindo linha 08.\n");
+    printf("Imprimindo linha 09.\n");
+    printf("Imprimindo linha 10.\n");
+    return 0;
+}
